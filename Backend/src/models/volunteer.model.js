27770ba888ddf0f810c1b4633ff005376e850af2
RPrashan99 <RashmithaMK@gmail.com>
2024-04-08 12:20:15 +0530

    Volunteer minor addition

--- Backend/src/models/volunteer.model.js
+++ Backend/src/models/volunteer.model.js
@@ -5,11 +5,14 @@ export const VolunteerSchema = new Schema(
         id: {type: String, required: true},
         fullName: {type: String, required: true},
         email: {type: String, required: true, unique: true},
+        nicNumber: {type: String, required: true, unique: true},
         phoneNumber: {type: String, required: true},
+        age: {type: Number, required: true},
         address: {type: String, required: true},
         skills: {type: [String], default: []},
         experience: {type: String, default: "None"},
-        motivation: {type: String, default: "None"}
+        motivation: {type: String, default: "None"},
+        createdDate: {type: String, required: true}
     },
     {
         timestamps: true,
