14fc95c933ceedb6c67644f2b741a258fa0c87dd
WijesekaraDNM <masha.wijesekara@gmail.com>
2024-06-26 14:40:19 +0530

    axiosConfig changes

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 
-    process.env.NODE_ENV != 'production'? 'http://localhost:5000' : 'https://dms-server-dgtg.onrender.com';
+    process.env.NODE_ENV != 'production'? 'http://localhost:5000' : 'https://dms-server-goil.onrender.com';
